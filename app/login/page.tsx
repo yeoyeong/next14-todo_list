107b99b7b3f130441a294c3f82833743167f101a
yeoyeong <yeoyyeong@gmail.com>
2023-12-06 11:49:54 +0900

    테스트 용 이메일 문구 삽입

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,9 @@ const page = () => {
   return (
     <div>
       <LoginForm />
+      <p>테스트 용</p>
+      <p>[임시 이메일 - zzz@zzz.com]</p>
+      <p>[비밀번호 - zxc]</p>
       <Link className="block w-full text-right" href="/signup">
         회원가입
       </Link>
